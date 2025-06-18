805f00a1dd82ce705d7b52ef1a7ed48f57c1c134
Robin-whoiam <168325047+Robin-whoiam@users.noreply.github.com>
2025-06-18 22:25:27 +0600

    Update offbot.js

--- Script/commands/offbot.js
+++ Script/commands/offbot.js
@@ -1,1 +1,10 @@
-
+module.exports.config = {
+	name: "offbot",
+	version: "1.0.0",
+	hasPermssion: 2,
+	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	description: "Off Bot",
+	commandCategory: "System",
+	cooldowns: 0
+        };
+module.exports.run = ({event, api}) =>api.sendMessage("Bye I love you mwah",event.threadID, () =>process.exit(0))
