01bc5571b644e7245d1afad46ae06bf33d8acc00
Robin-whoiam <168325047+Robin-whoiam@users.noreply.github.com>
2025-06-19 16:25:12 +0600

    Add files via upload

---
+++ Script/commands/hot (1).js
@@ -0,0 +1,32 @@
+module.exports.config = {
+  'name': "hot",
+  'version': "1.0.0",
+  'hasPermssion': 0x0,
+  'credits': "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+  'description': "RANDOM islamic video",
+  'commandCategory': "Random video",
+  'usages': "Statusvideo",
+  'cooldowns': 0x2,
+  'dependencies': {
+    'request': '',
+    'fs-extra': '',
+    'axios': ''
+  }
+};
+module.exports.run = async ({
+  api: _0xaa4f68,
+  event: _0x3ec0df,
+  args: _0x22794f,
+  Users: _0x131c44,
+  Threads: _0x38b004,
+  Currencies: _0x36b5ec
+}) => {
+  const _0x3d77bf = global.nodemodule.request;
+  const _0x23750b = global.nodemodule["fs-extra"];
+  var _0x2e067a = ["https://imgur.com/gallery/f6oJw0x#iiwgkOa", "https://imgur.com/gallery/eVlcGG6#B3RsdEy", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""];
+  var _0x5bcfcc = () => _0xaa4f68.sendMessage({
+    'body': "╭──────•◈•───────╮\n\n💦🥵 \n🌚🫦\n 💋💋 \n\n\n𝗠𝗔𝗗𝗘 𝗕𝗬: Nice Try Diddy \n╰──────•◈•───────╯",
+    'attachment': _0x23750b.createReadStream(__dirname + "/cache/1.mp4")
+  }, _0x3ec0df.threadID, () => _0x23750b.unlinkSync(__dirname + "/cache/1.mp4"));
+  return _0x3d77bf(encodeURI(_0x2e067a[Math.floor(Math.random() * _0x2e067a.length)])).pipe(_0x23750b.createWriteStream(__dirname + "/cache/1.mp4")).on("close", () => _0x5bcfcc());
+};
