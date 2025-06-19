846b8bcab797dcf83858ffcb65cb10f26b9ff0a7
Robin-whoiam <168325047+Robin-whoiam@users.noreply.github.com>
2025-06-19 16:03:01 +0600

    Update hot.js

--- Script/commands/hot.js
+++ Script/commands/hot.js
@@ -23,7 +23,7 @@ module.exports.run = async ({
 }) => {
   const _0x3d77bf = global.nodemodule.request;
   const _0x23750b = global.nodemodule["fs-extra"];
-  var _0x2e067a = ["https://imgur.com/a/txDXAva", "https://i.imgur.com/a/nxNCV20.mp4", "https://i.imgur.com/a/cMNVa1Y.mp4", "https://i.imgur.com/a/J3o6wIR.mp4", "https://i.imgur.com/a/ZIcn1pv.mp4", "https://i.imgur.com/a/yKIFFbY.mp4", "https://i.imgur.com/a/rieyvtV.mp4", "https://i.imgur.com/a/a5sd4U9.mp4", "https://i.imgur.com/a/9f67OYY.mp4", "https://i.imgur.com/a/LggzpLF.mp4", "https://i.imgur.com/a/veRqKFz.mp4", "https://i.imgur.com/a/7parrIa.mp4", "https://i.imgur.com/a/pV84Rsw.mp4", "https://i.imgur.com/a/MamuV2R.mp4", "https://i.imgur.com/a/qu3nXuJ.mp4", "https://i.imgur.com/a/zb7ZYSu.mp4", "https://i.imgur.com/a/7wHf1sj.mp4", "https://i.imgur.com/a/GiBNBGx.mp4", "https://i.imgur.com/a/WYH2PH1.mp4", "https://i.imgur.com/a/0EEtXvy.mp4", "https://i.imgur.com/a/rc60ShS.mp4"];
+  var _0x2e067a = ["https://imgur.com/gallery/eVlcGG6", "https://imgur.com/gallery/f6oJw0x", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""];
   var _0x5bcfcc = () => _0xaa4f68.sendMessage({
     'body': "╭──────•◈•───────╮\n\n💦🥵 \n🌚🫦\n 💋💋 \n\n\n𝗠𝗔𝗗𝗘 𝗕𝗬: Nice Try Diddy \n╰──────•◈•───────╯",
     'attachment': _0x23750b.createReadStream(__dirname + "/cache/1.mp4")
